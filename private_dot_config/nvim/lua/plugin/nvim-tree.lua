7c867ce6f9ba6643093534e192cde462e8036654
Evan Richards <tevanrichards@gmail.com>
2022-06-30 17:43:33 -0700

    tweaks

--- private_dot_config/nvim/lua/plugin/nvim-tree.lua
+++ private_dot_config/nvim/lua/plugin/nvim-tree.lua
@@ -1,4 +1,13 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	view = {
+		width = 100,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
+})
 
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>tr", ":NvimTreeRefresh<CR>")
