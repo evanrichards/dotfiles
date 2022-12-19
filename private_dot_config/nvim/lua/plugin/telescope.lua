3653c5d57c5924c56b6a4f6d47dd3e6867d3d648
Evan Richards <tevanrichards@gmail.com>
2022-12-18 19:54:13 -0800

    cleanup telescope init

--- private_dot_config/nvim/lua/plugin/telescope.lua
+++ private_dot_config/nvim/lua/plugin/telescope.lua
@@ -1,22 +1,23 @@
-local telescope = require('telescope')
+local telescope = require("telescope")
+local builtins = require("telescope.builtin")
 telescope.setup({
 	defaults = {
 		file_ignore_patterns = { "dist", "generated", "node_modules", "prisma/migrations" },
-
 	},
 })
 telescope.load_extension("ui-select")
 telescope.load_extension("gh")
 
+local nmap = function(keys, func, desc)
+	vim.keymap.set("n", keys, func, { desc = desc })
+end
 -- Find files using Telescope command-line sugar.
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>fr", ":Telescope resume<CR>")
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
-vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
-vim.keymap.set("n", "<leader>fs", ":Telescope lsp_document_symbols<CR>")
-vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>")
-vim.keymap.set("n", "<leader>f", ":Telescope<CR>")
-vim.keymap.set("n", "gd", ":Telescope lsp_definitions<CR>")
-vim.keymap.set("n", "gi", ":Telescope lsp_implementations<CR>")
-vim.keymap.set("n", "<leader>D", ":Telescope lsp_type_definitions<CR>")
-vim.keymap.set("n", "gr", ":Telescope lsp_references<CR>")
+nmap("<leader>ff", builtins.find_files, "[F]ind [F]iles")
+nmap("<leader>fr", builtins.resume, "[F]ind [R]esume")
+nmap("<leader>fg", builtins.live_grep, "[F]ind [G]rep")
+nmap("<leader>fb", builtins.buffers, "[F]ind [B]uffers")
+nmap("<leader>fs", builtins.lsp_document_symbols, "[F]ind [S]ymbols")
+nmap("<leader>fh", builtins.help_tags, "[F]ind [H]elp")
+nmap("<leader>f", builtins.builtin, "[F]ind something")
+nmap("gd", builtins.lsp_definitions, "[G]oto [D]efinition")
+nmap("gr", builtins.lsp_references, "[G]oto [R] references")
