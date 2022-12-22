dc51dcd24450bf4a44f31b1f972d2fa72d55dcbe
Evan Richards <tevanrichards@gmail.com>
2022-12-21 16:35:56 -0800

    fix preselect

--- private_dot_config/nvim/lua/plugin/lsp.lua
+++ private_dot_config/nvim/lua/plugin/lsp.lua
@@ -5,7 +5,9 @@ local nmap = require("keymap").nmap
 lsp.set_preferences({
 	suggest_lsp_servers = true,
 	setup_servers_on_start = true,
-	set_lsp_keymaps = true,
+	-- manage my own keymaps here for now because i want to use telescope for
+	-- some of the lsp stuff
+	set_lsp_keymaps = false,
 	configure_diagnostics = true,
 	cmp_capabilities = true,
 	manage_nvim_cmp = true,
@@ -29,6 +31,8 @@ lsp.configure("tsserver", {
 })
 
 lsp.on_attach(function(_, bufnr)
+	nmap("K", vim.lsp.buf.hover, "Display definition", bufnr)
+	nmap("<C-k>", vim.lsp.buf.signature_help, "Display signature", bufnr)
 	nmap("<leader>e", vim.diagnostic.open_float, "Open [E]rrors under cursor", bufnr)
 	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[N]ame symbol", bufnr)
 	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", bufnr)
@@ -43,8 +47,12 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 -- I want tab to be used by copilot, and have to manually select drop down items
 cmp_mappings["<Tab>"] = nil
 cmp_mappings["<S-Tab>"] = nil
-local cmp_config = lsp.defaults.cmp_config({ preselect = cmp.PreselectMode.None })
+local cmp_config = lsp.defaults.cmp_config()
 cmp_config["mapping"] = cmp_mappings
+cmp_config["preselect"] = "none"
+cmp_config["completion"] = {
+	completeopt = "menu,menuone,noinsert,noselect",
+}
 lsp.setup_nvim_cmp(cmp_config)
 
 cmp.setup.cmdline("/", {
