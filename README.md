16e525cdb39f098bbc286e7fb04c479c6e5c9c8e
Evan Richards <tevanrichards@gmail.com>
2023-08-03 10:54:00 -0700

    qid opener

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Setup should be easy:
 $ brew install chezmoi
 $ chezmoi init https://github.com/evanrichards/dotfiles.git
 ``````
+
+#QMK setup
+
+## Install QMK
+[Official Docs](https://docs.qmk.fm/#/newbs_getting_started)
+
+```sh
+brew install qmk/qmk/qmk
+# I needed to install the following manually for some reason
+# above @8 it complains
+brew tap osx-cross/avr
+brew install avr-gcc@8
+qmk setup evanrichards/qmk_firmware
+```
+
+
--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -67,3 +67,23 @@ vim.cmd("set makeprg=yarn\\ tsc")
 -- vim.cmd("autocmd FileType typescript setlocal errorformat=%+A %#%f %#(%l\\,%c): %m,%C%m")
 -- and we want to set the makeprg
 -- vim.cmd("autocmd FileType typescript setlocal makeprg=yarn\\ tsc\\ --noEmit")
+-- Function to get visual selection
+function _G.get_visual_selection()
+	local _, start_row, start_col, _ = unpack(vim.fn.getpos("'<"))
+	local _, end_row, end_col, _ = unpack(vim.fn.getpos("'>"))
+	if start_row ~= end_row then
+		return ""
+	end
+	local line = vim.api.nvim_buf_get_lines(0, start_row - 1, start_row, false)[1]
+	return line:sub(start_col, end_col)
+end
+
+-- Function to open a URL
+function _G.open_url()
+	local selection = _G.get_visual_selection()
+	local url = "https://go/qid/" .. selection
+	vim.fn.jobstart({ "open", url })
+end
+
+-- Map visual mode command to open url
+vim.api.nvim_set_keymap("v", "<leader>u", ":lua open_url()<CR>", { noremap = true, silent = true })
