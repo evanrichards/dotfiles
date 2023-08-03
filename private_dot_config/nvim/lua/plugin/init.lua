08fe91648f6fdeff711e3bfc149eab035cf0dc0a
Evan Richards <tevanrichards@gmail.com>
2023-08-03 12:13:16 -0700

    implement tsc plugin

--- private_dot_config/nvim/lua/plugin/init.lua
+++ private_dot_config/nvim/lua/plugin/init.lua
@@ -280,4 +280,23 @@ require("packer").startup(function(use)
 			require("wpm").setup({})
 		end,
 	})
+	use({
+		"dmmulroy/tsc.nvim",
+		config = function()
+			require("tsc").setup({
+				auto_open_qflist = true,
+				auto_close_qflist = false,
+				bin_path = vim.fn.findfile("node_modules/.bin/tsc"),
+				enable_progress_notifications = true,
+				flags = {
+					noEmit = true,
+					project = function()
+						return vim.fn.findfile("tsconfig.json")
+					end,
+				},
+				hide_progress_notifications_from_history = true,
+				spinner = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
+			})
+		end,
+	})
 end)
