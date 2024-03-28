ae750ed7559153650cc0a89bbc82f0393325a05f
Evan Richards <tevanrichards@gmail.com>
2024-03-28 00:11:40 -0700

    clean up open tmux sh

--- open-tmux-buffer-as-qid.sh
+++ open-tmux-buffer-as-qid.sh
@@ -11,33 +11,39 @@ else
   RAW_TEXT=$(tmux show-buffer | xargs)
 fi
 
+# Function to open a file in Vim
+open_file_in_vim() {
+  local file_path="$1"
+  if [[ -e "$file_path" ]]; then
+    tmux split-window -h -p 50 -c "$CURRENT_DIR" "zsh -i -c 'nvim $file_path'"
+  fi
+}
+
+# Function to open a URL
+open_url() {
+  local url="$1"
+  open "$url"
+}
+
 # Adjusted regular expression to match a URL up to the first whitespace
 URL_REGEX="(http|https):\/\/[^[:space:]]+"
 
 # Use grep to find and extract the first URL up to a whitespace in RAW_TEXT
 URL=$(echo "$RAW_TEXT" | grep -oE "$URL_REGEX")
 
-# Check if there is an absolute file path in the buffer with grep
-FILE_PATH=$(echo "$RAW_TEXT" | grep -oE '\/[a-zA-Z0-9_\/\.-]+')
+# Check if there is a file path (absolute or relative) in the buffer
+FILE_PATH=$(echo "$RAW_TEXT" | grep -oE '[a-zA-Z0-9_\/\.-]+\.(c|cpp|java|py|sh|rb|pl|php|js|html|css|scss|json|xml|yml|yaml|md|txt|ts|tsx)')
 
 if [[ -n "$URL" ]]; then
   # If a URL is extracted, open it
-  open "$URL"
-elif [[ -d "$FILE_PATH" ]]; then
-  tmux split-window -h -p 50 -c "$CURRENT_DIR" "zsh -i -c 'nvim $FILE_PATH'"
-# if its a a code file, open it in vim
-elif [[ -f "$FILE_PATH" && "$FILE_PATH" =~ \.(c|cpp|java|py|sh|rb|pl|php|js|html|css|scss|json|xml|yml|yaml|md|txt|ts|tsx)$ ]]; then
-  tmux split-window -h -p 50 -c "$CURRENT_DIR" "zsh -i -c 'nvim $FILE_PATH'"
+  open_url "$URL"
+elif [[ -n "$FILE_PATH" ]]; then
+  # If a file path is found, open it in Vim
+  open_file_in_vim "$FILE_PATH"
 else
   # Loop through each line that matches the structured identifier pattern
   echo "$RAW_TEXT" | grep -oE 'qid::[a-z_]+:[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}' | while read -r QID; do
-    if [[ -n "$QID" ]]; then
-      URL="https://go/qid/$QID"
-      open "$URL"
-    else
-      echo "No valid QID found in buffer."
-      echo "Buffer content: $(tmux show-buffer)"
-      exit 1
-    fi
+    URL="https://go/qid/$QID"
+    open_url "$URL"
   done
 fi
