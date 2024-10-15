53688d06a3ffcf581b62dce6bfc1366395b90f10
Evan Richards <tevanrichards@gmail.com>
2024-10-15 11:08:16 -0700

    add avante

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -147,7 +147,7 @@ return {
 				on_attach = on_attach,
 				capabilities = capabilities,
 			})
-			lspconfig["tsserver"].setup({
+			lspconfig["ts_ls"].setup({
 				on_attach = on_attach,
 				capabilities = capabilities,
 				init_options = {
--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 local nmap = require("helpers.keys").nmap
 local vmap = require("helpers.keys").vmap
-local getVisualSelection = require("helpers.visual_selection")
 return {
 	{
 		"nvim-telescope/telescope.nvim",
@@ -12,14 +11,31 @@ return {
 			"sharkdp/fd",
 			"nvim-tree/nvim-web-devicons",
 			"nvim-telescope/telescope-ui-select.nvim",
+			"nvim-telescope/telescope-live-grep-args.nvim",
 			{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
 		},
 		config = function()
 			local telescope = require("telescope")
 			local builtins = require("telescope.builtin")
+			local lga_actions = require("telescope-live-grep-args.actions")
+			local actions = require("telescope.actions")
 			telescope.setup({
 				defaults = {
 					file_ignore_patterns = { "dist", "generated", "node_modules", "prisma/migrations" },
+				},
+
+				extensions = {
+					live_grep_args = {
+						auto_quoting = true, -- enable/disable auto-quoting
+						mappings = { -- extend mappings
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+								-- freeze the current list and start a fuzzy search in the frozen list
+								["<C-space>"] = actions.to_fuzzy_refine,
+							},
+						},
+					},
 				},
 			})
 			pcall(telescope.load_extension, "fzf")
@@ -27,12 +43,12 @@ return {
 			telescope.load_extension("gh")
 			telescope.load_extension("chezmoi")
 			telescope.load_extension("notify")
+			telescope.load_extension("live_grep_args")
 			-- Find files using Telescope command-line sugar.
 			nmap("<leader>ff", builtins.find_files, "[F]ind [F]iles")
 			nmap("<leader>fc", telescope.extensions.chezmoi.find_files, "[F]ind [C]hezmoi files")
 			nmap("<leader>?", builtins.oldfiles, "[?] Find recently opened files")
 			nmap("<leader>fr", builtins.resume, "[F]ind [R]esume")
-			nmap("<leader>fg", builtins.live_grep, "[F]ind [G]rep")
 			nmap("<leader>fb", builtins.buffers, "[F]ind [B]uffers")
 			nmap("<leader>fq", builtins.quickfix, "[F]ind [Q]uickfix")
 			nmap("<leader>fs", function()
@@ -46,10 +62,10 @@ return {
 			nmap("gd", builtins.lsp_definitions, "[G]oto [D]efinition")
 			nmap("gr", builtins.lsp_references, "[G]oto [R]eferences")
 			vmap("<leader>fg", function()
-				local selection = getVisualSelection()
-				builtins.grep_string({
-					search = selection,
-				})
+				require("telescope-live-grep-args.shortcuts").grep_visual_selection()
+			end, "[F]ind [G]rep")
+			nmap("<leader>fg", function()
+				require("telescope").extensions.live_grep_args.live_grep_args()
 			end, "[F]ind [G]rep")
 		end,
 	},
